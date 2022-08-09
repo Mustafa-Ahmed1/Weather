981ae6888e3cb46aead4e60f4b8f49a297973e52
Mustafa Ahmed <mstfyjmalalden@gmail.com>
2022-08-09 15:10:16 +0300

    add ui states for loading and error states, and improve activity_main layout

--- app/src/main/java/com/mustafa/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/mustafa/weatherapp/ui/MainActivity.kt
@@ -18,8 +18,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val client = OkHttpClient()
     private val httpUrl = buildHttpUrl()
-    private val LOG_TAG = "MainActivity"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -60,9 +58,7 @@ class MainActivity : AppCompatActivity() {
             textTemperatureNow.text = "${currentValues.temperature}°"
             textTemperatureToday.text = weather.data.timelines[0].intervals[3].values.temperature
             textWindSpeedToday.text = currentValues.windSpeed
-            textRainToday.text = currentValues.humidity
-            textWeatherDescription.text =
-                weatherCodeMap[weather.data.timelines[0].intervals[0].values.weatherCode]
+            textHumidityToday.text = currentValues.humidity
             textTodayDate.text = "Today, $formattedDate"
         }
     }
@@ -91,32 +87,8 @@ class MainActivity : AppCompatActivity() {
             .build()
     }
 
-    private val weatherCodeMap = mapOf(
-        "0" to "Unknown",
-        "1000" to "Clear, Sunny",
-        "1100" to "Mostly Clear",
-        "1101" to "Partly Cloudy",
-        "1102" to "Mostly Cloudy",
-        "1001" to "Cloudy",
-        "2000" to "Fog",
-        "2100" to "Light Fog",
-        "4000" to "Drizzle",
-        "4001" to "Rain",
-        "4200" to "Light Rain",
-        "4201" to "Heavy Rain",
-        "5000" to "Snow",
-        "5001" to "Flurries",
-        "5100" to "Light Snow",
-        "5101" to "Heavy Snow",
-        "6000" to "Freezing Drizzle",
-        "6001" to "Freezing Rain",
-        "6200" to "Light Freezing Rain",
-        "6201" to "Heavy Freezing Rain",
-        "7000" to "Ice Pellets",
-        "7101" to "Heavy Ice Pellets",
-        "7102" to "Light Ice Pellets",
-        "8000" to "Thunderstorm"
-    )
+    companion object {
+        const val LOG_TAG = "MainActivity"
+    }
+
 }
-
-
